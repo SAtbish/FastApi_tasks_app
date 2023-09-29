bc87a56817e5c56b4319eff9d307d4405bf081f1
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 02:30:54 +0300

    Refactored users pydantic schemes

--- src/schemas/users.py
+++ src/schemas/users.py
@@ -1,20 +1,32 @@
 from pydantic import BaseModel, Field, EmailStr, constr
 
+from src.schemas.base import ResponseModel
 
-class UserSchema(BaseModel):
-    id: int
+
+class UserResponse(BaseModel):
     name: str
     login: str
-    password: str
     email: str
     is_confirmed: bool
+
+
+class UserSchema(UserResponse):
+    id: int
+    password: str
 
     class Config:
         from_attributes = True
 
 
-class UserRegistration(BaseModel):
-    name: str = Field(min_length=1, max_length=64, description="User name")
+class UserLogin(BaseModel):
     login: str = Field(min_length=1, max_length=64, description="User login")
     password: constr(pattern="^[A-Za-z0-9-!№;$%^&*():?/|.,~`]+$", min_length=8, max_length=64)
+
+
+class UserRegistration(UserLogin):
+    name: str = Field(min_length=1, max_length=64, description="User name")
     email: EmailStr
+
+
+class UserResponseModel(ResponseModel):
+    data: UserResponse
