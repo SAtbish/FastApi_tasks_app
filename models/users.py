43ab6bb2b134b2d1f3173914cb548a909017828e
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-29 19:44:12 +0300

    Add Users table

---
+++ models/users.py
@@ -0,0 +1,10 @@
+from sqlalchemy.orm import Mapped, mapped_column
+from db.db import Base
+
+
+class Users(Base):
+    __tablename__ = "users"
+
+    id: Mapped[int] = mapped_column(primary_key=True)
+    name: Mapped[str]
+    email: Mapped[str]
