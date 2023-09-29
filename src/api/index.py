5dba0eb642a73895266d56c23c2390a93c97653e
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-29 23:04:41 +0300

    Add index_handler

---
+++ src/api/index.py
@@ -0,0 +1,24 @@
+from main import app
+from fastapi.responses import JSONResponse
+from fastapi import status
+from src.schemas.base import ResponseModel
+from datetime import datetime
+
+route_description = {
+    'response_model': ResponseModel,
+    'status_code': status.HTTP_200_OK,
+    'tags': ["index"],
+    'description': (
+        "Проверяет доступно ли приложение\n\n"
+        "Check availability of application"
+    )
+
+}
+
+
+@app.get("/", **route_description)
+@app.get("/ping", **route_description)
+async def index_handler():
+    return JSONResponse(
+        content=ResponseModel(message=f"{datetime.now()}: Working...").__dict__,
+        status_code=status.HTTP_200_OK)
