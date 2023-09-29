f41beb1c8d1beb2e27fa242aecbb1232bbc333d2
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 00:44:50 +0300

    Add CORS for app

--- main.py
+++ main.py
@@ -1,11 +1,18 @@
 import uvicorn
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 
 app = FastAPI(
     title="Приложение для ведения задач пользователей"
 )
-
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 if __name__ == "__main__":
     uvicorn.run(app="src.api:app", reload=True)
