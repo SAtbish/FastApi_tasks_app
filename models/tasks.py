4007e2ddddf565b50e4ada153685dde5e61f4c42
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-29 21:19:13 +0300

    Update Tasks Table

--- models/tasks.py
+++ models/tasks.py
@@ -9,8 +9,10 @@ class Tasks(Base):
     __tablename__ = "tasks"
 
     id: Mapped[int] = mapped_column(primary_key=True)
-    name: Mapped[str]
-    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    assignee_id: Mapped[int] = mapped_column(ForeignKey("users.id"))
-    creation_date: Mapped[datetime]
+    title: Mapped[str] = mapped_column(nullable=False)
+    author_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    assignee_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
+    creation_date: Mapped[datetime] = mapped_column(nullable=False)
     expiration_date: Mapped[datetime]
+    is_done: Mapped[bool] = mapped_column(default=False)
+    done_date: Mapped[datetime]
