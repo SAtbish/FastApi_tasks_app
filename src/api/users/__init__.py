e44825f5268a36777f80c8b826a270968b9fb5ec
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 20:16:36 +0300

    Add update_user_password_handler

--- src/api/users/__init__.py
+++ src/api/users/__init__.py
@@ -4,6 +4,7 @@ from src.api.users.get_all import router
 from src.api.users.get import router
 from src.api.users.delete import router
 from src.api.users.update_info import router
+from src.api.users.update_password import router
 
 
 __all__ = ["router"]
