5aa2df92cddbebb49f9eb731bcd1a447948eb785
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 14:27:29 +0300

    Add get_user_handler

--- src/api/users/__init__.py
+++ src/api/users/__init__.py
@@ -1,6 +1,7 @@
 from src.api.users.registration import router
 from src.api.users.login import router
 from src.api.users.get_all import router
+from src.api.users.get import router
 
 
 __all__ = ["router"]
