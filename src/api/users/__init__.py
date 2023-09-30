6fe157d690977454f2a14bd714bcbd98ab08d2e7
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 19:09:57 +0300

    Add update_user_info_handler

--- src/api/users/__init__.py
+++ src/api/users/__init__.py
@@ -3,6 +3,7 @@ from src.api.users.login import router
 from src.api.users.get_all import router
 from src.api.users.get import router
 from src.api.users.delete import router
+from src.api.users.update_info import router
 
 
 __all__ = ["router"]
