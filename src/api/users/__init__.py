572f3046f9228bd3e5a7de2b53513ff93197eed0
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-09-30 23:12:43 +0300

    Add get_all_users_paginated_handler

--- src/api/users/__init__.py
+++ src/api/users/__init__.py
@@ -1,6 +1,7 @@
 from src.api.users.registration import router
 from src.api.users.login import router
 from src.api.users.get_all import router
+from src.api.users.get_all_paginated import router
 from src.api.users.get import router
 from src.api.users.delete import router
 from src.api.users.update_info import router
---
+++ src/api/users/get_all_paginated.py
@@ -0,0 +1,24 @@
+from src.api.users.router import router
+from fastapi import status
+from fastapi_pagination import Page
+
+from src.schemas.users import UserSchema
+from src.api.dependencies import UOWDep
+from src.services.users import UsersService
+
+
+@router.post(
+    "/get/all/paginated",
+    status_code=status.HTTP_200_OK,
+    description=(
+            "Получает всех пользователей с пагинацией\n\n"
+            "Get all users paginated"
+    ),
+    summary="getting users paginated",
+    response_model=Page[UserSchema]
+)
+async def get_all_users_paginated_handler(
+        uow: UOWDep
+):
+    users = await UsersService().get_all_paginated(uow)
+    return users
