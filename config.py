edfa6d978e2f9658bbf55a78b2c770704cc2587b
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-01 22:24:32 +0300

    Add new env values in config.py

--- config.py
+++ config.py
@@ -12,7 +12,16 @@ if not load_dotenv():
         "\nDB_USER={DB_USER}"
         "\nDB_PSW={DB_PSW}"
         "\nDB_NAME={DB_NAME}"
-        "\nTOKEN_UPDATE_TIME={TOKEN_UPDATE_TIME}"
+        "\n\n#REDIS CONFIG"
+        "\nREDIS_NAME={REDIS_NAME}"
+        "\nREDIS_HOST={REDIS_HOST}"
+        "\nREDIS_PORT={REDIS_PORT}"
+        "\nREDIS_DB={REDIS_DB}"
+        "\nREDISMAXPULL={REDISMAXPULL}"
+        "\n\nTOKEN_UPDATE_TIME={TOKEN_UPDATE_TIME}"
+        "\n\n#SMTP CONFIG"
+        "\nEMAIL_SENDER={EMAIL_SENDER}"
+        "\nSMTP_PASSWORD={SMTP_PASSWORD}"
     )
 
     exit(404)
@@ -24,3 +33,13 @@ DB_PSW = os.getenv("DB_PSW")
 DB_NAME = os.getenv("DB_NAME")
 
 TOKEN_UPDATE_TIME = int(os.getenv("TOKEN_UPDATE_TIME"))
+
+REDIS_NAME = os.getenv("REDIS_NAME")
+REDIS_HOST = os.getenv("REDIS_HOST")
+REDIS_PORT = os.getenv("REDIS_PORT")
+REDIS_DB = os.getenv("REDIS_DB")
+REDISMAXPULL = os.getenv("REDISMAXPULL")
+REDIS_NOTIFICATIONS_KEY = os.getenv("REDIS_NOTIFICATIONS_KEY")
+
+EMAIL_SENDER = os.getenv("EMAIL_SENDER")
+SMTP_PASSWORD = os.getenv("SMTP_PASSWORD")
