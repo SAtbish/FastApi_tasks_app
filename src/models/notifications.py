0aa588a73b1505eade72c9dea2a769c51d9e4860
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-01 22:29:34 +0300

    Change Notifications attributes

--- src/models/notifications.py
+++ src/models/notifications.py
@@ -3,6 +3,7 @@ from datetime import datetime
 from sqlalchemy.orm import Mapped, mapped_column
 from sqlalchemy import ForeignKey
 from src.db.db import Base
+from src.schemas.notifications import NotificationSchema
 
 
 class Notifications(Base):
@@ -11,8 +12,6 @@ class Notifications(Base):
     id: Mapped[int] = mapped_column(primary_key=True)
     header: Mapped[str] = mapped_column(nullable=False)
     message: Mapped[str] = mapped_column(nullable=False)
-    type: Mapped[int] = mapped_column(ForeignKey("notification_types.type"), nullable=False)
+    type: Mapped[str] = mapped_column(ForeignKey("notification_types.type"), nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     sent_datetime: Mapped[datetime] = mapped_column(nullable=False)
-    is_read: Mapped[bool] = mapped_column(default=False)
-    read_datetime: Mapped[datetime]
