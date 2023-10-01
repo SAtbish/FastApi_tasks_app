02a4670bfffc6994e4b93bc1e54fa5930df0dd32
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-01 22:29:54 +0300

    Add to_read_model for Notifications table

--- src/models/notifications.py
+++ src/models/notifications.py
@@ -15,3 +15,13 @@ class Notifications(Base):
     type: Mapped[str] = mapped_column(ForeignKey("notification_types.type"), nullable=False)
     user_id: Mapped[int] = mapped_column(ForeignKey("users.id"), nullable=False)
     sent_datetime: Mapped[datetime] = mapped_column(nullable=False)
+
+    def to_read_model(self) -> NotificationSchema:
+        return NotificationSchema(
+            id=self.id,
+            header=self.header,
+            message=self.message,
+            type=self.type,
+            user_id=self.user_id,
+            sent_datetime=self.sent_datetime
+        )
