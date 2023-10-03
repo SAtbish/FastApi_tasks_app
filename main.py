398ab3360a8b71f5edfb32578ec09b0875fb4449
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-03 23:38:27 +0300

    Make middleware authorization

--- main.py
+++ main.py
@@ -1,14 +1,19 @@
+from starlette.requests import Request
 import uvicorn
-from fastapi import FastAPI
+from fastapi import FastAPI, status
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.responses import JSONResponse
 from fastapi_pagination import add_pagination
 from fastapi_cache import FastAPICache
 from fastapi_cache.backends.redis import RedisBackend
+
+from src.utils.user_authorization import user_authorization
 from src.redis_worker.redis_worker import r
 from src.tasks.celery_worker import celery_app
 from multiprocessing import Process
 from subprocess import Popen
 
+NOT_AUTHORIZATION_PATHS = ["/", "/docs", "/openapi.json", "/users/registration", "/users/login"]
 
 app = FastAPI(
     title="Приложение для ведения задач пользователей"
@@ -27,6 +32,22 @@ add_pagination(app)
 async def startup_event():
     FastAPICache.init(RedisBackend(r), prefix="fastapi-cache")
 
+
+@app.middleware("http")
+async def add_process_time_header(request: Request,  call_next):
+    tokens = {}
+
+    if request.url.path not in NOT_AUTHORIZATION_PATHS:
+        tokens, err = await user_authorization(request)
+        if not tokens:
+            return JSONResponse(status_code=status.HTTP_401_UNAUTHORIZED, content={'message': err})
+
+    response = await call_next(request)
+    if tokens:
+        for key, value in tokens.items():
+            response.set_cookie(key, value)
+
+    return response
 
 if __name__ == "__main__":
     celery_worker_process = Process(target=celery_app.worker_main,
