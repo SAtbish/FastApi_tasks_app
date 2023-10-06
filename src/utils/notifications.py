492b167af1789e2cf8c72ebc0ce7fa7de8d4ae2a
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-07 00:55:46 +0300

    Add make_new_task_notification for generating email message

--- src/utils/notifications.py
+++ src/utils/notifications.py
@@ -3,7 +3,10 @@ import hashlib
 from src.schemas.notifications import NotificationRedisModel
 
 confirm_email_header = "Please confirm your email."
-confirm_email_message = lambda email, user_id: f'<div><h1 style="color: red;">Здравствуйте, Click here: http://127.0.0.1:8000/users/confirm/email/{user_id}/{hashlib.md5(bytes(email, "utf-8")).hexdigest()}</h1></div>'
+confirm_email_message = lambda email, user_id: f'<div><h1 style="color: red;">Click here: http://127.0.0.1:8000/users/confirm/email/{user_id}/{hashlib.md5(bytes(email, "utf-8")).hexdigest()}</h1></div>'
+
+new_task_header = "New task for you."
+new_task_message = "There is new task for you!"
 
 
 def make_confirm_email_notification(email: str, user_id: int) -> NotificationRedisModel:
@@ -15,3 +18,10 @@ def make_confirm_email_notification(email: str, user_id: int) -> NotificationRed
     )
 
 
+def make_new_task_notification(user_id: int) -> NotificationRedisModel:
+    return NotificationRedisModel(
+        header=new_task_header,
+        message=new_task_message,
+        type="new_task",
+        user_id=user_id
+    )
