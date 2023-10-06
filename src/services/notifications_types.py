2fdacdfb6d1c2b6fd2929f10b9a54c8c9bc5ffb7
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-06 03:43:21 +0300

    Add NotificationsTypesService

---
+++ src/services/notifications_types.py
@@ -0,0 +1,13 @@
+from src.utils.unitofwork import IUnitOfWork
+
+
+class NotificationsTypesService:
+    @staticmethod
+    async def create_notification_types(uow: IUnitOfWork):
+        async with uow:
+            result = await uow.notification_types.get_all()
+            if not result:
+                await uow.notification_types.create_one(
+                    data={"type": "confirm_email", "description": "confirm email"}
+                )
+            await uow.commit()
