c1ee0329b337704e5ec5193bf096963cb17b968e
Alexey <97658978+SAtbish@users.noreply.github.com>
2023-10-07 00:55:16 +0300

    Add Tasks pydantic models

--- src/schemas/tasks.py
+++ src/schemas/tasks.py
@@ -1,13 +1,30 @@
+from pydantic import Field
 from datetime import datetime
-from pydantic import BaseModel
+from pydantic import BaseModel, field_validator
+
+from src.schemas.base import ResponseModel
 
 
-class TasksSchema(BaseModel):
-    id: int
-    title: str
-    author_id: int
-    assignee_id: int
+class CreateTasks(BaseModel):
+    title: str = Field(min_length=1, max_length=64, description="Task title")
+    author_id: int = Field(gt=0, description="Task author")
+    assignee_id: int = Field(gt=0, description="Task assignee")
+    expiration_date: datetime | None = None
+    is_done: bool | None = False
+    done_date: datetime | None = None
+
+    @field_validator('expiration_date', 'done_date')
+    def name_must_contain_space(cls, date: datetime) -> datetime:
+        return date.replace(tzinfo=None)
+
+
+class TaskData(CreateTasks):
     creation_date: datetime
-    expiration_date: datetime
-    is_done: bool
-    done_date: datetime
+
+
+class TaskResponse(ResponseModel):
+    data: TaskData
+
+
+class TasksSchema(TaskData):
+    id: int
